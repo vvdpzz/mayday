8de6fd2d01f18e7d727aa5b78eda3589a55390fa
VvDPzZ <vvdpzz@gmail.com>
2011-01-31 00:49:42 +0800

    init commit

---
+++ Gemfile
@@ -0,0 +1,13 @@
+source 'http://rubygems.org'
+
+gem 'rails', '3.0.3'
+
+group :development, :test do
+  gem 'mysql2'
+  gem 'devise'
+  gem 'acts-as-taggable-on'
+  gem 'bluecloth'
+  gem 'gravtastic'
+  gem 'jquery-rails'
+  gem 'rails3-jquery-autocomplete'
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Mayday::Application.config.secret_token = '1ac7983d88ecdfd240e8eb707fe497e4850c1687cd77916a2e31f6666467c7a98ae2f5ec7a32f387f58a00f2ce17ff02c7c3f0654b199c1572f436bdca232b76'
