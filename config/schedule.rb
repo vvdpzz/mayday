2846ae3f2982fad29be0650c603ff9b87b77a5a1
VvDPzZ <vvdpzz@gmail.com>
2011-02-23 08:26:06 +0800

    fix schedule

--- config/schedule.rb
+++ config/schedule.rb
@@ -19,6 +19,6 @@
 
 # Learn more: http://github.com/javan/whenever
 
-every 1.day, :at => '5:00 am' do
-  rake "-s sitemap:refresh"
+every 1.day, :at => '3:00 am' do
+  rake "-s sitemap:refresh RAILS_ENV=production"
 end
