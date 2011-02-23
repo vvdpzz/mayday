ec9e94a931847f4b3b26fa25bbd8dd85bbefbe42
VvDPzZ <vvdpzz@gmail.com>
2011-02-23 11:35:56 +0800

    answer js

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -5,4 +5,4 @@ $.fn.extend({charCounter:function(a)
 else
 {if(d<g){c=(g-d)+" more to go..";h(false)}
 else{if(i){c=(i-d)+" character"+(i-d!=1?"s":"")+" left";h(d<=i)}}}
-var f=$(this).parents("form").find("span.text-counter");f.text(c);if(!f.hasClass(e)){f.removeClass("warning supernova hot warm cool").addClass(e)}})})}});$(document).ready(function(){var u=false;var p=function(w){u=w};$("#question_body").charCounter({min:5,max:140,setIsValid:p});$("#answer_body").charCounter({min:15,setIsValid:p});$(".new_comment textarea").charCounter({min:5,max:140,setIsValid:p});});
+var f=$(this).parents("form").find("span.text-counter");f.text(c);if(!f.hasClass(e)){f.removeClass("warning supernova hot warm cool").addClass(e)}})})}});$(document).ready(function(){var u=false;var p=function(w){u=w};$("#question_body").charCounter({min:5,max:140,setIsValid:p});$(".answer_body").charCounter({min:15,setIsValid:p});$(".new_comment textarea").charCounter({min:5,max:140,setIsValid:p});});
